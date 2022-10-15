f6cbd2318e8c2e8244f8b07d81e93786ae066650
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-10-15 17:52:45 +0300

    create mock and task model

---
+++ src/app/mock-task.ts
@@ -0,0 +1,26 @@
+import { Task } from "./Task"
+
+export const TASK:Task[]=[{
+  id:1,
+  text:'Doctors Appointment',
+  day:'May 5th at 2:30px',
+  reminder:true,
+},
+{
+  id:2,
+  text:'Meeting at School',
+  day:'May 6th at 1:30px',
+  reminder:true,
+},
+{
+  id:3,
+  text:'Food Shopping',
+  day:'May 7th at 12:30px',
+  reminder:false,
+},
+{
+  id:4,
+  text:'Football Matches  Appointment',
+  day:'May 8th at 9:30px',
+  reminder:false,
+},]
