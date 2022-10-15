5fa667deddd53a24d89088c5e17dab0334f07f51
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-10-15 18:20:40 +0300

    create task-item and pass the data.

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+  @Input() task?:Task;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
