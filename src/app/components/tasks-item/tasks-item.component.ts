5a677a9040e41d1753aaac24b49d5f541fc8dc6c
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-10-15 19:14:39 +0300

    Added font-awesome and pass the data taskitem comp

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../../Task';
-
+import { faTimes} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-tasks-item',
   templateUrl: './tasks-item.component.html',
@@ -8,6 +8,7 @@ import { Task } from '../../Task';
 })
 export class TasksItemComponent implements OnInit {
   @Input() task?:Task;
+  faTimes=faTimes;
   constructor() { }
 
   ngOnInit(): void {
