e137fc81a765d145724051b126849f559b327bbc
enes baykal gurbuz <enesgurbuz@lastikcim.com.tr>
2022-10-16 14:33:26 +0300

    Toggle button and ui service added.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  title:string = 'crash-angular';
-  constructor() { }
-
-  ngOnInit(): void {
+  title: string = 'crash-angular';
+  showAddTask?: boolean =true;
+  subscription?: Subscription;
+  constructor(private uiService: UiService) {
+    this.subscription=this.uiService.onToggle().subscribe(value=>this.showAddTask=value);
   }
-  toggleAddTesk(){
-    console.log("Toggle")
+
+  ngOnInit(): void {}
+  toggleAddTesk() {
+    this.uiService.toggleAddTask();
   }
 }
